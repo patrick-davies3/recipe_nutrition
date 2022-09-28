1e411b47909c297b28a4df425e9b291ca62fbd17
PATRICK DAVIES <patrick.davies@daugherty.com>
2022-09-28 09:20:38 -0500

    update

--- recipe_nutrition.py
+++ recipe_nutrition.py
@@ -6,3 +6,6 @@ streamlit.title('Recipe Nutrition Calculator')
 scraper = scrape_me('https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/')
 ingredients_df = pandas.DataFrame(scraper.ingredients())
 streamlit.dataframe(ingredients_df)
+
+nutrition_df = pandas.DataFrame(scraper.nutrients)
+streamlit.dataframe(nutrition_df)
