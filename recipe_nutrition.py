fcc9d018eb0261e6a1b6cbddc71a07fc08c5013f
PATRICK DAVIES <patrick.davies@daugherty.com>
2022-09-28 10:52:54 -0500

    same

--- recipe_nutrition.py
+++ recipe_nutrition.py
@@ -22,7 +22,8 @@ macros = [nutrients.get('carbohydrateContent').strip(' g'), nutrients.get('fatCo
 labels = 'Carbs', 'Fat', 'Protein'
 
 fig1, ax1 = plt.subplots()
-ax1.pie(macros, labels=labels, autopct='%1.1f%%')
+ax1.pie(macros, labels=labels, autopct='%1.1f%%',
+        startangle=-90)
 
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
