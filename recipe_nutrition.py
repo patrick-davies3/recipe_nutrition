5c1f4bd6d021494f0b7ecd8bb521ea7c272202c5
PATRICK DAVIES <patrick.davies@daugherty.com>
2022-09-28 10:45:49 -0500

    adding pie chart

--- recipe_nutrition.py
+++ recipe_nutrition.py
@@ -1,6 +1,7 @@
 import streamlit
 from recipe_scrapers import scrape_me
 import pandas as pd
+import matplotlib.pyplot as plt
 
 streamlit.title('Recipe Nutrition Calculator')
 scraper = scrape_me('https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/')
@@ -10,7 +11,19 @@ streamlit.dataframe(ingredients_df)
 
 streamlit.text(scraper.nutrients())
 streamlit.text(type(scraper.nutrients()))
-nutrition_df = pd.Series(scraper.nutrients(), name ='Value')
+nutrients = scraper.nutrients()
+nutrition_df = pd.Series(nutrients, name ='Value')
 nutrition_df.index.name = 'Type'
 nutrition_df.reset_index()
 streamlit.dataframe(nutrition_df)
+macros = [nutrients.get('carbohydrateContent'), nutrients.get('fatContent'),nutrients.get('proteinContent')]
+
+labels = 'Carbs', 'Fat', 'Protein'
+
+fig1, ax1 = plt.subplots()
+ax1.pie(macros, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+streamlit.pyplot(fig1)
