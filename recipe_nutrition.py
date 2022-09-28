989d771508c326fbcf2afc4c5965b6203dc8b61f
PATRICK DAVIES <patrick.davies@daugherty.com>
2022-09-28 09:57:06 -0500

    trying series

--- recipe_nutrition.py
+++ recipe_nutrition.py
@@ -1,15 +1,16 @@
 import streamlit
 from recipe_scrapers import scrape_me
-import pandas
+import pandas as pd
 
 streamlit.title('Recipe Nutrition Calculator')
 scraper = scrape_me('https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/')
-ingredients_df = pandas.DataFrame(scraper.ingredients())
+ingredients_df = pd.DataFrame(scraper.ingredients())
 streamlit.dataframe(ingredients_df)
 
 
 streamlit.text(scraper.nutrients())
 streamlit.text(type(scraper.nutrients()))
-nutrition_df = pandas.DataFrame.from_dict(scraper.nutrients(), orient='index',
-                       columns=['Type', 'Value']).index([0])
+nutrition_df = pd.DataFrame.Series(scraper.nutrients(), name ='Value')
+nutrition_df.index.name = 'Type'
+nutrition_df.reset_index()
 streamlit.dataframe(nutrition_df)
