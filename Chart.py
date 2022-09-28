66000b531c4a1b6d3b1ee12583c6d5f1aeac9fc5
PATRICK DAVIES <patrick.davies@daugherty.com>
2022-09-28 11:19:56 -0500

    separating pie chart functionality

---
+++ Chart.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+def makeMacroChart(nutrients):
+    macros = [nutrients.get('carbohydrateContent').strip(' g'), nutrients.get('fatContent').strip(' g'), 
+            nutrients.get('proteinContent').strip(' g')]
+
+    labels = 'Carbs', 'Fat', 'Protein'
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(macros, labels=labels, autopct='%1.1f%%',
+        startangle=-90)
+
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    return fig1
--- recipe_nutrition.py
+++ recipe_nutrition.py
@@ -2,6 +2,7 @@ import streamlit
 from recipe_scrapers import scrape_me
 import pandas as pd
 import matplotlib.pyplot as plt
+import Chart as ch
 
 streamlit.title('Recipe Nutrition Calculator')
 scraper = scrape_me('https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/')
@@ -16,15 +17,6 @@ nutrition_df = pd.Series(nutrients, name ='Value')
 nutrition_df.index.name = 'Type'
 nutrition_df.reset_index()
 streamlit.dataframe(nutrition_df)
-macros = [nutrients.get('carbohydrateContent').strip(' g'), nutrients.get('fatContent').strip(' g'), 
-            nutrients.get('proteinContent').strip(' g')]
 
-labels = 'Carbs', 'Fat', 'Protein'
 
-fig1, ax1 = plt.subplots()
-ax1.pie(macros, labels=labels, autopct='%1.1f%%',
-        startangle=-90)
-
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-streamlit.pyplot(fig1)
+streamlit.pyplot(ch.makeMacroChart(nutrients))
